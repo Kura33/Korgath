6eca19d44e1854d0dd30ea78d2135aeed7822d32
Sébastien XAVIER CARLOS <sebastien.xaviercarlos@hotmail.com>
2019-11-12 22:04:25 +0100

    add labs life

--- Korgath/GameViewController.swift
+++ Korgath/GameViewController.swift
@@ -15,31 +15,33 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
-        }
+//        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+//        // including entities and graphs.
+//        if let scene = GKScene(fileNamed: "GameScene") {
+//
+//            // Get the SKScene from the loaded GKScene
+//            if let sceneNode = scene.rootNode as! GameScene? {
+//
+//                // Copy gameplay related content over to the scene
+//                sceneNode.entities = scene.entities
+//                sceneNode.graphs = scene.graphs
+//
+//                // Set the scale mode to scale to fit the window
+//                sceneNode.scaleMode = .aspectFill
+//
+//                // Present the scene
+//                if let view = self.view as! SKView? {
+//                    view.presentScene(sceneNode)
+//
+//                    view.ignoresSiblingOrder = true
+//
+//                    view.showsFPS = true
+//                    view.showsNodeCount = true
+//                }
+//            }
+//        }
+        
+        pvActualHero(damage: 0)
     }
 
     override var shouldAutorotate: Bool {
@@ -57,4 +59,38 @@ class GameViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+//    début de mon code spécifique
+    
+    var pvMax = ["pvMaxHero": 100, "pvMaxEnnemi": 100]
+     var pvActual = ["pvHero": 100, "pvEnnemi": 100]
+     
+     var damage = 0
+         
+     @IBOutlet weak var heroLife: UILabel!
+     @IBOutlet weak var ennemiLife: UILabel!
+
+     
+     func pvActualHero(damage: Int) -> Int {
+        pvActual["pvHero"]! -= damage
+
+//         let quoteLifeH = "\(String(describing: pvActual["pvHero"]!)) / \(String(describing: pvMax["pvMaxHero"]!))"
+         
+         heroLife.text = "test"
+         
+         return pvActual["pvHero"]!
+         
+     }
+     
+    func pvActualEnnemi(damage: Int) -> Int { pvActual["pvEnnemi"]! -= damage
+     
+     let quoteLifeE = "\(String(describing: pvActual["pvEnnemi"])) / \(String(describing: pvMax["pvMaxEnnemi"]))"
+     
+     ennemiLife.text = quoteLifeE
+     
+        return pvActual["pvEnnemi"]!
+     
+     }
+    
+    
 }
